2307792f1f095d4bb63e69c7d6be02348e0c41b5
MURAT CAKMAK <MURAT.CAKMAK2@lcwaikiki.com>
2020-04-26 15:04:28 +0300

    Add Create method in Course Servicee

--- CleanArch.Application/Services/CourseService.cs
+++ CleanArch.Application/Services/CourseService.cs
@@ -1,5 +1,7 @@
 ﻿using CleanArch.Application.Interfaces;
 using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Commands;
+using CleanArch.Domain.Core.Bus;
 using CleanArch.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -9,13 +11,27 @@ namespace CleanArch.Application.Services
 {
     public class CourseService : ICourseService
     {
-        private ICourseRepository _courseRepository;
-        public CourseService(ICourseRepository courseRepository)
+        private readonly ICourseRepository _courseRepository;
+        private readonly IMediatorHandler _mediatorHandler;
+        public CourseService(ICourseRepository courseRepository, IMediatorHandler mediatorHandler)
         {
             _courseRepository = courseRepository;
+            _mediatorHandler = mediatorHandler;
         }
+
+        public void Create(CourseViewModel courseViewModel)
+        {
+            var command = new CreateCourseCommand(
+                courseViewModel.Name, 
+                courseViewModel.Description, 
+                courseViewModel.ImageUrl);
+
+            _mediatorHandler.SendCommand(command);
+        }
+
         public CourseViewModel GetCourses()
         {
+
             return new CourseViewModel()
             {
                 Courses = _courseRepository.GetCourses()
